9d2319ffc4ea9ca0b4e7e944100b48de6b72f53a
ehila <ehila@redhat.com>
2025-11-14 12:08:17 -0500

    feat: add yamlfmt support
    added pre-commit check to catch shellcheck and yamlfmt errors
    added pre-commit install and uninstall tasks to Makefile
    added yamlfmt script and config to format and verify yaml files
    
    Signed-off-by: ehila <ehila@redhat.com>

---
+++ hack/yamlfmt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+CONTAINER_ENGINE=${CONTAINER_ENGINE:-podman}
+CONTAINER_IMAGE="ghcr.io/google/yamlfmt:latest"
+FMT_CONFIG=".yamlfmt"
+VALIDATE_ONLY=${VALIDATE_ONLY:-false}
+VALIDATE_ONLY_FLAG_ARGS="--lint"
+EXTRA_FLAG_ARGS=""
+
+if [ "$VALIDATE_ONLY" != "false" ]; then
+  EXTRA_FLAG_ARGS="$VALIDATE_ONLY_FLAG_ARGS"
+fi
+
+
+if [ "$OPENSHIFT_CI" != "" ]; then
+  TOP_DIR="${1:-.}"
+  yamlfmt -conf "$FMT_CONFIG" "$EXTRA_FLAG_ARGS" "$TOP_DIR"
+else
+  $CONTAINER_ENGINE run --rm \
+    --env OPENSHIFT_CI=TRUE \
+    --env VALIDATE_ONLY="$VALIDATE_ONLY" \
+    --volume "${PWD}:/workdir:z" \
+    --entrypoint sh \
+    --workdir /workdir \
+    $CONTAINER_IMAGE \
+    hack/yamlfmt.sh "${@}"
+fi;
